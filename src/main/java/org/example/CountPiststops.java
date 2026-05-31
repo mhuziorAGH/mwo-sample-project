48450942682716fc11f0261a8d1272f8da2fbf69
Michał Huzior <mhuzior@student.agh.edu.pl>
2026-05-31 12:45:55 +0200

    Add count class

---
+++ src/main/java/org/example/CountPiststops.java
@@ -0,0 +1,13 @@
+package org.example;
+
+import java.util.List;
+
+public class CountPiststops {
+    public double calculatePiststops(List<Pitstop> pitstops) {
+
+        double total = 0;
+        for (Pitstop pitstop : pitstops) {
+
+        }
+    }
+}
