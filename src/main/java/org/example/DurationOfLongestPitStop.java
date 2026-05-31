7fbc69b87c009fab8da536d45477989507216e85
Lila <lila.88.xx@gmail.com>
2026-05-31 12:05:02 +0200

    DurationOfLongestPitStop

--- src/main/java/org/example/DurationOfLongestPitStop.java
+++ src/main/java/org/example/DurationOfLongestPitStop.java
@@ -1,11 +1,23 @@
 package org.example;
 
+import java.util.List;
+
+
 //czas trwania najdłuższego pit stopu wraz z numerem kierowcy
 
 
 public class DurationOfLongestPitStop {
 
+    public static void findLongestPitStop(List<Pitstop> pitstops) {
+
+        Pitstop longestPitStop = pitstops.get(0);
+
+        for (Pitstop p : pitstops) {
+            if (p.getPit_duration() > longestPitStop.getPit_duration()) {
+                longestPitStop = p;
+            }
+        }
 
 
-
+    }
 }
